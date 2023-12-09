129b52c51504efda78ec40c2d0cfe4edb1166b34
anwnie <anwnie@gmail.com>
2023-12-09 17:14:48 +0400

    final boxplot for regions and hummidity & minor changes for world_map

--- boxplot.py
+++ boxplot.py
@@ -1,22 +1,32 @@
-import matplotlib.pyplot as plt
+from filter import FilterOption, filter
+from tools import get_column_mean
 from csv_reader import CsvReader
+import matplotlib.pyplot as plt
+import numpy as np
 
 dataset = CsvReader('GlobalWeatherRepository.csv')
 
-plt.figure(figsize=(10, 6))
-data = []
+plt.figure(figsize=(10, 6), facecolor="darkgrey", edgecolor="black")
+
+regions_list = []
+humidity_list = []
+
 for row in dataset.rows:
-    data.append(row.get("temperature_celsius"))
-plt.boxplot(data)
-plt.title("Boxplot of Average Monthly Temperature")
-plt.xlabel("Time")
-plt.ylabel("Temperature (°C)")
+    reg = row.get("timezone").split("/")[0]
+    if reg not in regions_list:
+        regions_list.append(reg)
 
-date = []
-for row in dataset.rows:
-    date.append(row.get("last_updated"))
+for i in range(len(regions_list)):
+    humidity_values = []
+    for row in dataset.rows:
+        if row.get("timezone").split("/")[0] == regions_list[i]:
+            humidity_values.append(row.get("humidity"))
 
+    humidity_list.append(humidity_values)
 
+plt.boxplot(humidity_list, labels=regions_list)
+plt.xticks(rotation=20)
+plt.title("Box Plot of Average Humidity by Regions")
+plt.xlabel("Region")
+plt.ylabel("Humidity (%)")
 plt.show()
-
-
