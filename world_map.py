39d5602c15a28dde4de1daa8aa02efbba256fa73
anwnie <anwnie@gmail.com>
2023-12-14 17:08:00 +0400

    2

--- world_map.py
+++ world_map.py
@@ -1,18 +1,22 @@
 import math
 import matplotlib.pyplot as plt
-from csv_reader import CsvReader
+from csv_reader import CsvReader  # Assuming you have a CSV reader module
 import matplotlib.patches as patches
-from filter import FilterOption, filter
+from filter import FilterOption, filter  # Assuming you have a filtering module
 
+# Setting the style for the plot
 plt.style.use('dark_background')
+
+# Loading the dataset
 dataset = CsvReader('GlobalWeatherRepository.csv')
 
+# Creating subplots and loading the map image
 fig, ax = plt.subplots()
 img = plt.imread('visuals/map.png')
 ax.imshow(img, extent=[-180, 180, -90, 90])
 
 
-# define a function that formats the coordinates
+# Defining a function to format coordinates
 def format_coord(x, y):
     """
     Format coordinates: lon=x.xx, lat=y.yy.
@@ -20,17 +24,21 @@ def format_coord(x, y):
     return f'lon={x:.2f}, lat={y:.2f}'
 
 
+# Assigning the format_coord function to the axes
 ax.format_coord = format_coord
 ax.set_xlabel('Latitude')
 ax.set_ylabel('Longitude')
 
+# Plotting temperature points on the map based on temperature ranges
 for row in dataset.rows:
-    # get the latitude and longitude values
+    # Getting latitude and longitude values
     lat = row.get('latitude')
     lon = row.get('longitude')
 
+    # Getting the temperature variable
     temp = row.get('temperature_celsius')
-    #   temp is the temperature variable
+
+    # Assigning colors based on temperature ranges and plotting on the map
     if temp > 40:
         ax.text(lon, lat, '+', color='darkred')
     elif 30 < temp <= 40:
@@ -46,4 +54,5 @@ for row in dataset.rows:
     else:
         ax.text(lon, lat, '+', color='darkblue')
 
+# Displaying the plot
 plt.show()
