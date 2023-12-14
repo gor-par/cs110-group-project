4a3a82408650e56b1123c70434c32abba46d009d
anwnie <anwnie@gmail.com>
2023-12-14 17:03:41 +0400

    improved

--- boxplot.py
+++ boxplot.py
@@ -1,22 +1,28 @@
 import numpy as np
-from filter import FilterOption, filter
-from tools import get_column_mean
-from csv_reader import CsvReader
 import matplotlib.pyplot as plt
+from csv_reader import CsvReader
+from tools import get_column_mean
+from filter import FilterOption, filter
 
-plt.style.use('dark_background')
+# Loading the dataset
 dataset = CsvReader('GlobalWeatherRepository.csv')
 
+# Setting the style for the plot
+plt.style.use('dark_background')
+
+# Creating a new figure for the plot
 plt.figure(figsize=(10, 6), edgecolor="black")
 
 regions_list = []
 humidity_list = []
 
+# Extracting unique region names from the dataset
 for row in dataset.rows:
     reg = row.get("timezone").split("/")[0]
     if reg not in regions_list:
         regions_list.append(reg)
 
+# Populating the humidity_list with values for each region
 for i in range(len(regions_list)):
     humidity_values = []
     for row in dataset.rows:
@@ -25,9 +31,12 @@ for i in range(len(regions_list)):
 
     humidity_list.append(humidity_values)
 
+# Creating a box plot with humidity values for each region
 plt.boxplot(humidity_list, labels=regions_list)
 plt.xticks(rotation=20)
 plt.title("Box Plot of Average Humidity by Regions")
 plt.xlabel("Region")
 plt.ylabel("Humidity (%)")
+
+# Displaying the plot
 plt.show()
